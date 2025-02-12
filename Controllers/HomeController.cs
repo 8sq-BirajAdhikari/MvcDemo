2f3b45a98035ec4cd826d3867851aa81e2f783d6
8sq-BirajAdhikari <biraj.adhikari@8squarei.com>
2025-02-12 09:07:15 +0545

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcDemo.Models;
+using System.Diagnostics;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(WeeklyForecast);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static List<Weather> WeeklyForecast = new()
+        {
+            new Weather { Day = "Monday", Condition = "Sunny", TemperatureCelsius = 25 },
+            new Weather { Day = "Tuesday", Condition = "Partly Cloudy", TemperatureCelsius = 22 },
+            new Weather { Day = "Wednesday", Condition = "Rainy", TemperatureCelsius = 18 },
+            new Weather { Day = "Thursday", Condition = "Thunderstorms", TemperatureCelsius = 20 },
+            new Weather { Day = "Friday", Condition = "Windy", TemperatureCelsius = 19 },
+            new Weather { Day = "Saturday", Condition = "Overcast", TemperatureCelsius = 21 },
+            new Weather { Day = "Sunday", Condition = "Sunny", TemperatureCelsius = 26 }
+        };
+    }
+}
---
+++ Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcDemo.Models
+{
+    public class Weather
+    {
+        public string? Day { get; set; }
+        public string? Condition { get; set; }
+        public decimal TemperatureCelsius { get; set; }
+    }
+}
